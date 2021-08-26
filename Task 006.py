f6e7bc311e27d89f635930086c50023afa1e5ad9
NVDuc <nvduc.ds@hotmail.com>
2021-08-27 06:33:42 +0700

    Add file Task 006

---
+++ Task 006.py
@@ -0,0 +1,17 @@
+"""
+Write a method that calculates the square of a number.
+
+Suggestions:
+Utilize the ** operator.
+--- Nguyen Van Duc ---
+"""
+x = int(input("Enter a number: "))
+
+
+def square(num):
+    return num ** 2
+
+
+print(square(2))  # print square of 2
+print(square(3))  # print square of 3
+print(square(x))  # print square of x
