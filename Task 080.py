6d6355743cdfda440128994b499d31e15b1e966a
NVDuc <nvduc.ds@hotmail.com>
2021-09-04 14:42:53 +0700

    Change note!

--- Task 080.py
+++ Task 080.py
@@ -1,5 +1,6 @@
 """
-Question 080 - Please write a program to shuffle  and print the list [3,6,7,8].
+Question 080 - Level 02
+Please write a program to shuffle  and print the list [3,6,7,8].
 Hints: Use shuffle() function to shuffle a list.
 --- Nguyen Van Duc ---
 """
