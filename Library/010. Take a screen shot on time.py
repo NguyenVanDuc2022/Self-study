4f88e193c4af827fa99256dd6fd22eb9211b9f56
NVDuc <nvduc.ds@hotmail.com>
2021-09-24 15:44:57 +0700

    "Fist commit"

---
+++ Library/010. Take a screen shot on time.py
@@ -0,0 +1,20 @@
+import pyautogui
+from datetime import datetime
+import time
+
+Time1 = "15:36:50"
+Time2 = "15:36:53"
+t = 0
+while t < 2:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+    if current_time == Time1:
+        screenshot = pyautogui.screenshot()
+        screenshot.save("FirstPicAt_" + Time1.replace(":", "") + ".png")
+        t += 1
+    elif current_time == Time2:
+        screenshot = pyautogui.screenshot()
+        screenshot.save("SecondPicAt_" + Time2.replace(":", "") + ".png")
+        t += 1
+    time.sleep(1)
